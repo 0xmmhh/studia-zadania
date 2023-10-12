cebc90b30c77b18f98503caf1e1151e950cb07cc
Maciej Hawrot <synekoraja1922@gmail.com>
2023-10-12 16:03:17 +0200

    to the power of (?)

---
+++ powers.py
@@ -0,0 +1,8 @@
+def powers(x, y):
+    return x ** y
+
+podstawa = int(input("Podaj podstawę: "))
+potega = int(input("Podaj potęgę: "))
+wynik = (powers(podstawa, potega))
+
+print(f"Wynik {podstawa} do potęgi {potega} wynosi {wynik}")
