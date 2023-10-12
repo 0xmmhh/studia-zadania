f009ee1241a1e26f97f4647600f94c3b8dcf0cd2
Maciej Hawrot <synekoraja1922@gmail.com>
2023-10-12 15:38:26 +0200

    how many kilometers?

---
+++ unit_conversion.py
@@ -0,0 +1,6 @@
+print("Enter the number of miles to see how many kilometers it would be")
+miles = float(input("Please type in how many miles: "))
+
+kilometers = miles * 1.609344
+
+print(f"{miles} miles is {kilometers} kilometers")
